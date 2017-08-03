70e6acbe5f52927c40457e3638a0032b497285cb
Dallin Johnson <dallin.b.johnson@gmail.com>
2017-08-03 00:17:26 -0600

    Added API connections and created the API for proposals then fixed some styles and the create new proposals API calls and functions

--- freelance-camp-fe/app/app.module.ts
+++ freelance-camp-fe/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { DocumentService } from './documents/document.service';
 import { ProposalListComponent } from './proposal/proposal-list.component';
 import { ProposalNewComponent } from './proposal/proposal-new.component';
 import { ProposalShowComponent } from './proposal/proposal-show.component';
+import { ProposalService } from './proposal/proposal.service';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -18,7 +20,8 @@ import { AppRoutingModule } from './app-routing.module';
 		BrowserModule,
 		AppRoutingModule,
 		FormsModule,
-		NgbModule.forRoot()
+		NgbModule.forRoot(),
+		HttpModule
 	],
 	declarations: [
 		AppComponent,
@@ -29,7 +32,8 @@ import { AppRoutingModule } from './app-routing.module';
 		ProposalShowComponent
 	],
 	providers: [
-		DocumentService
+		DocumentService,
+		ProposalService
 	],
 	bootstrap: [
 		AppComponent
